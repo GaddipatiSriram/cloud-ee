a455ede14366598b033d606aa710b974c43cbb08
Sriram Gaddipati <sgaddipati@hnvjkw62mm.mynetworksettings.com>
2024-01-19 15:37:58 -0500

    updte

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ gsed -i 's/FROM/FROM --platform=linux\/amd64/' context/Dockerfile
 export REGISTRY=quay.io/sriramgaddipati
 cd context/
 docker buildx build --no-cache --platform linux/arm64,linux/amd64 --build-arg ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN=$ANSIBLE_GALAXY_SERVER_AUTOMATION_HUB_TOKEN -t $REGISTRY/cloud-ee:latest --push .
+cd ..
+rm -rfv context
